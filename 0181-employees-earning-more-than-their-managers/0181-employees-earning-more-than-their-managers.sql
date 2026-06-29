999ed4f63f57ed6a5d673066fe15a1fbf48dee16
Megha Harikumar P <meghaharikumar2005@gmail.com>
2026-06-29 20:16:21 +0530

    ADDING ANOTHER QUERY
    Added a second query to retrieve employees earning more than their managers.

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
+  
 SELECT e2.name employee
 from employee e1
 inner join employee e2 on e1.id =e2.managerId
 where e1.salary<e2.salary;
+
+SELECT E2.name AS Employee
+FROM Employee E1
+JOIN Employee E2
+ON E1.id = E2.managerId
+WHERE E2.salary > E1.salary;
