a8551e1fb273b62e407bd340d96fe092b3d94c8f
Megha Harikumar P <meghaharikumar2005@gmail.com>
2026-07-03 09:19:43 +0530

    Update SQL query for employees earning more than managers
    Refactored SQL query to improve readability and consistency.

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,12 +1,20 @@
 # Write your MySQL query statement below
-  
+     #1
 SELECT e2.name employee
 from employee e1
 inner join employee e2 on e1.id =e2.managerId
 where e1.salary<e2.salary;
-
+      #2
 SELECT E2.name AS Employee
 FROM Employee E1
 JOIN Employee E2
 ON E1.id = E2.managerId
 WHERE E2.salary > E1.salary;
+      #3
+SELECT e1.name Employee
+FROM Employee e1
+WHERE e1.salary > (
+    SELECT e2.salary
+    FROM Employee e2
+    WHERE e2.id = e1.managerId
+);
