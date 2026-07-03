51ca561c01a11d1ac347b7908d5b1ceeb4784fe3
Megha Harikumar P <meghaharikumar2005@gmail.com>
2026-07-03 10:39:02 +0530

    Update 0196-delete-duplicate-emails.sql

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -25,5 +25,9 @@ WHERE p1.email = p2.email
 
 -- So:
 
--- smaller id = winner (keep)
+DELETE p1
+FROM Person p1
+JOIN Person p2
+ON p1.email=p2.email
+AND p1.id>p2.id; -- smaller id = winner (keep)
 -- bigger id = loser (delete)
