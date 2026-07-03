a728cf826588728e3e0a07f494367a3e5765ad2a
Megha Harikumar P <meghaharikumar2005@gmail.com>
2026-07-03 10:39:45 +0530

    Update comments for clarity in SQL delete query
    Clarified comments in SQL for deleting duplicate emails.

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -25,9 +25,11 @@ WHERE p1.email = p2.email
 
 -- So:
 
+-- smaller id = winner (keep)
+-- bigger id = loser (delete)
+
 DELETE p1
 FROM Person p1
 JOIN Person p2
 ON p1.email=p2.email
-AND p1.id>p2.id; -- smaller id = winner (keep)
--- bigger id = loser (delete)
+AND p1.id>p2.id; 
